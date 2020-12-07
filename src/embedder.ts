67aa1fe455b21fb69679d8c8b3e1b81122f8f81d
Felix Becker <felix.b@outlook.com>
2020-12-07 19:15:41 +0100

    fix: recreate iframe to make sure cookies are sent
    Fixes cookies missing after previewing the second link.

---
+++ src/embedder.ts
@@ -0,0 +1,17 @@
+import { Message } from './messages'
+
+window.addEventListener('message', event => {
+	const message = event.data as Message
+	if (message.method === 'preview') {
+		console.log('Received preview message in embedder frame for URL', message.linkUrl)
+		// We need to recreate the iframe so that the origin is "fresh" and will send Cookies properly with SameSite=Lax.
+		// For some reason updating the `src` of an existing iframe will not cause the Cookies for the updated origin to be sent.
+		document.querySelector<HTMLIFrameElement>('#preview-iframe')?.remove()
+		const iframe = document.createElement('iframe')
+		iframe.id = 'preview-iframe'
+		document.body.append(iframe)
+		iframe.src = message.linkUrl
+		return
+	}
+	throw new Error('Unknown message ' + message.method)
+})
